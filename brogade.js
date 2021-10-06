be4e255556f62dcfb29608b7d3d6fbfd32929782
AniketDehnikar <89062728+AniketDehnikar@users.noreply.github.com>
2021-10-06 15:04:26 +0530

    Update brogade.js

--- brogade.js
+++ brogade.js
@@ -1,6 +1,7 @@
 const { events, Job, Groups } = require("brigadier");
 events.on("exec", () => {
   var build = new Job("build-job");
-  build.tasks = ["docker build -t myimage:10 dockerfile ."];
+  build.tasks = ["docker build -t aniketdehnikar11/ myimage:10 dockerfile .",
+                 "docker push aniketdehnikar11/ myimage:10"];
   build.run();
 })
